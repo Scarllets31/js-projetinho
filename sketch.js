3aadb88bb20c985ce9fcd70c68ba9d8cacbcd8b8
Scarllets31 <leticiamedeiroscosta31@Gmail.com>
2024-05-13 20:15:48 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let sound;
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  sound = loadSound('path/to/your/soundfile.mp3');
+}
+
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(800, 400);
+  sound.play();
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    stroke(255);
+    strokeWeight(5);
+    line(pmouseX, pmouseY, mouseX, mouseY);
   }
-  
-  function draw() {
-    background(220);
-  }
+}
